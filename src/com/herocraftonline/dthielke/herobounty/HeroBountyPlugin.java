b0b659e7d98169d643b7068475bc0eb6b4830db7
DThielke <dave.thielke@gmail.com>
2011-03-04 13:01:31 -0500

    Added licensing information. Now using BukkitScheduler rather than a java timer.

--- src/com/herocraftonline/dthielke/herobounty/HeroBountyPlugin.java
+++ src/com/herocraftonline/dthielke/herobounty/HeroBountyPlugin.java
@@ -1,3 +1,11 @@
+/**
+ * Copyright (C) 2011 DThielke <dave.thielke@gmail.com>
+ * 
+ * This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License.
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/3.0/ or send a letter to
+ * Creative Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA.
+ **/
+
 package com.herocraftonline.dthielke.herobounty;
 
 import java.io.File;
@@ -184,7 +192,7 @@ public class HeroBountyPlugin extends JavaPlugin {
 
         if (checkIConomy()) {
             TimerTask expirationChecker = new ExpirationChecker(this);
-            expirationTimer.scheduleAtFixedRate(expirationChecker, EXPIRATION_TIMER_DELAY, EXPIRATION_TIMER_PERIOD);
+            getServer().getScheduler().scheduleAsyncRepeatingTask(this, expirationChecker, EXPIRATION_TIMER_DELAY, EXPIRATION_TIMER_PERIOD);
         } else {
             logger.log(Level.WARNING, "iConomy not found. Disabling.");
             pm.disablePlugin(this);
@@ -618,8 +626,10 @@ class ExpirationChecker extends TimerTask {
 
     @Override
     public void run() {
-        for (Bounty bounty : plugin.getBounties()) {
-            for (String name : bounty.getExpirations().keySet()) {
+        Bounty[] bounties = plugin.getBounties().toArray(new Bounty[0]);
+        for (Bounty bounty : bounties) {
+            String[] hunters = bounty.getExpirations().keySet().toArray(new String[0]);
+            for (String name : hunters) {
                 if (bounty.getMillisecondsLeft(name) <= 0) {
                     Player hunter = plugin.getServer().getPlayer(name);
 
